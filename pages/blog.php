6463efdcf99ecbb542bfb961e2610c1f98073568
ronan <rlozahic@hotmail.fr>
2018-01-15 22:28:33 +0100

    video 3

--- pages/blog.php
+++ pages/blog.php
@@ -4,12 +4,31 @@
 $posts = get_posts();
 
 foreach($posts as $post){
-	print_r($post);
+	?>
+	<div class="row">
+		<div class="col s12 m12 l12">
+			<h4><?= $post ->title ?></h4>
+			<div class="row col s12 m6 l8">
+				<?= substr(nl2br($post->content),0,1200)?> 
+			</div>
+			<div class="row col s12 m6 l4">
+				<img src="img/posts/<?= $post->image ?>" class="materialboxed responsive-img" alt="<?= $post->title ?>"/>
+				<br />
+				<br />
+				<a class="btn light-blue waves-effect" href="index.php?page=post&id=<?= $post->id ?>">Lire l'article complet</a>
+			</div>
+		</div>
+	</div>
+
+	<?php
 }
 
-foreach($posts as $post){
-	echo $post->title."<br/>";
-}
 
 
-?>
+
+
+
+
+
+
+
